05b6fc2d369fa8203d0739fbb4482a69d82e0a86
Calum Macdonald <calmacx@gmail.com>
2024-10-04 11:59:51 +0100

    trying to get tests working again

--- tests/test_ted_app.py
+++ tests/test_ted_app.py
@@ -10,14 +10,16 @@ import helpers
 
 client = TestClient(ted)
 
+
 def test_read_status():
     response = client.get("/status")
     assert response.status_code == 200
     assert response.json() == {"message": "Resource Available"}
 
-def test_preprocess_dataset():
+
+async def test_preprocess_dataset():
     test_dataset = helpers.get_test_dataset()
-    document = preprocess_dataset(test_dataset)
+    document = await preprocess_dataset(test_dataset)
     assert isinstance(document, str)
     # Test that specific phrases from the summary appear in the document
     assert document.find("a test dataset") != -1
@@ -26,6 +28,7 @@ def test_preprocess_dataset():
     # Test structural data is not duplicated
     assert document.count("column") == 1
     assert document.count("table") == 1
+
 
 def test_extract_medical_entities():
     fake_annotations = helpers.get_test_annotations()
@@ -37,10 +40,11 @@ def test_extract_medical_entities():
     assert len(other_terms) == 1
     assert "2" in other_terms.keys()
     assert "3" not in other_terms.keys()
-    
-#Comment out if MVCM enabled
-@patch('ted_app.main.requests.post')
-# @pytest.mark.xfail 
+
+
+# Comment out if MVCM enabled
+@patch("ted_app.main.requests.post")
+# @pytest.mark.xfail
 def test_index_dataset(mock_post):
     mock_responses = [Mock(), Mock()]
     mock_responses[0].json.return_value = helpers.get_test_medcat_response()
@@ -53,30 +57,32 @@ def test_index_dataset(mock_post):
     assert response.status_code == 200
 
     response_dict = response.json()
+
     assert response_dict == {
         "id": "1111",
         "extracted_terms": [
             "191044006",
             "362969004",
             "73211009",
-            "Data Set", 
-            "Diabetes", 
+            "Data Set",
+            "Diabetes",
             "Diabetes mellitus",
             "Diabetes mellitus (disorder)",
             "Disorder of endocrine system",
-        ]
+        ],
     }
-    
+
+
 # Comment out if MVCM enabled
-@patch('ted_app.main.requests.post')
-# @pytest.mark.xfail 
+@patch("ted_app.main.requests.post")
+# @pytest.mark.xfail
 def test_index_datasets(mock_post):
     mock_responses = [Mock(), Mock()]
     mock_responses[0].json.return_value = helpers.get_test_bulk_medcat_response()
     mock_responses[1].json.return_value = helpers.get_test_mvcm_response()
     mock_post.side_effect = mock_responses
 
-    test_dataset = helpers.get_test_json_dataset() 
+    test_dataset = helpers.get_test_json_dataset()
 
     response = client.post("/datasets_bulk", json=[test_dataset, test_dataset])
     assert response.status_code == 200
@@ -84,15 +90,15 @@ def test_index_datasets(mock_post):
     response_arr = response.json()
     for dataset_resp in response_arr:
         assert dataset_resp == {
-        "id": "1111", 
-        "extracted_terms": [
-            "191044006",
-            "362969004",
-            "73211009",
-            "Data Set", 
-            "Diabetes",
-            "Diabetes mellitus",
-            "Diabetes mellitus (disorder)",
-            "Disorder of endocrine system",
-        ]
-    }
+            "id": "1111",
+            "extracted_terms": [
+                "191044006",
+                "362969004",
+                "73211009",
+                "Data Set",
+                "Diabetes",
+                "Diabetes mellitus",
+                "Diabetes mellitus (disorder)",
+                "Disorder of endocrine system",
+            ],
+        }
